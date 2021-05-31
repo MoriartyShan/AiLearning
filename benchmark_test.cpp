f1055f89506208e8f6e917bd21448401d7f10acd
moriarty <hitguohang@126.com>
2021-05-31 19:06:37 +0800

    test eigen and viennacl

--- benchmark_test.cpp
+++ benchmark_test.cpp
@@ -4,14 +4,47 @@
 #include <benchmark/benchmark.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/cudaarithm.hpp>
+#include <Eigen/Dense>
+#include <Eigen/Core>
 
-const int rows = 784, cols = 100;
+#include <viennacl/matrix.hpp>
+#include "viennacl/linalg/prod.hpp"
+
+//#include "viennacl/linalg/cuda/matrix_operations.hpp"
+#include "viennacl/tools/random.hpp"
+
+const int rows = 784, cols = 100, rows2 = cols, cols2 = 1;
 const double alpha = 11.3, belta = 1.5;
-
+using Scalar_ = float;
+#define CV_TYPE CV_32FC1
 #define LOG std::cout << __FILE__ << ":" << __LINE__ << ": " << time(0) << ","
 
+
+void test_viennacl_linalg_cuda(benchmark::State& state) {
+  viennacl::matrix<Scalar_> m1(rows, cols), m2(rows2, cols2), m3(rows, cols2), m4(rows, cols2);
+  viennacl::tools::uniform_random_numbers<Scalar_> randomNumber;
+#define RandomViennacl(matrix) do { \
+  for (unsigned int i = 0; i < matrix.size1(); ++i)\
+    for (unsigned int j = 0; j < matrix.size2(); ++j)\
+      matrix(i,j) = randomNumber();\
+} while(0)
+
+
+  RandomViennacl(m1);
+  RandomViennacl(m2);
+  RandomViennacl(m3);
+#undef RandomViennacl
+
+  for (auto _ : state) {
+//    viennacl::linalg::cuda::detail::prod(m1, false, m2, false, m4, 1, 1);
+//    viennacl::linalg::cuda::detail::add
+//    m4 *= alpha;
+//    m4 =  m3;
+  }
+}
+
 static void cpugpumulnostream(benchmark::State& state) {
-  cv::Mat m1(rows, cols, CV_64FC1), m2(rows, cols, CV_64FC1), m3(rows, cols, CV_64FC1);
+  cv::Mat m1(rows, cols, CV_TYPE), m2(rows, cols, CV_TYPE), m3(rows, cols, CV_TYPE);
   cv::randu(m1, -100, 100);
   cv::randu(m2, -100, 100);
   cv::randu(m3, -100, 100);
@@ -23,7 +56,7 @@ static void cpugpumulnostream(benchmark::State& state) {
 
 static void cpugpumul(benchmark::State& state) {
   cv::cuda::Stream stream;
-  cv::Mat m1(rows, cols, CV_64FC1), m2(rows, cols, CV_64FC1), m3(rows, cols, CV_64FC1);
+  cv::Mat m1(rows, cols, CV_TYPE), m2(rows, cols, CV_TYPE), m3(rows, cols, CV_TYPE);
   cv::randu(m1, -100, 100);
   cv::randu(m2, -100, 100);
   cv::randu(m3, -100, 100);
@@ -34,7 +67,7 @@ static void cpugpumul(benchmark::State& state) {
 }
 
 static void gpumulnostream(benchmark::State& state) {
-  cv::Mat tmp(rows, cols, CV_64FC1);
+  cv::Mat tmp(rows, cols, CV_TYPE);
   cv::cuda::GpuMat m1, m2, m3;
 
   cv::randu(tmp, -100, 100);
@@ -52,7 +85,7 @@ static void gpumulnostream(benchmark::State& state) {
 
 static void gpumul(benchmark::State& state) {
   cv::cuda::Stream stream;
-  cv::Mat tmp(rows, cols, CV_64FC1);
+  cv::Mat tmp(rows, cols, CV_TYPE);
   cv::cuda::GpuMat m1, m2, m3;
 
   cv::randu(tmp, -100, 100);
@@ -70,7 +103,7 @@ static void gpumul(benchmark::State& state) {
 }
 
 static void cpumul(benchmark::State& state) {
-  cv::Mat m1(rows, cols, CV_64FC1), m2(rows, cols, CV_64FC1), m3(rows, cols, CV_64FC1);
+  cv::Mat m1(rows, cols, CV_TYPE), m2(rows, cols, CV_TYPE), m3(rows, cols, CV_TYPE);
   cv::randu(m1, -100, 100);
   cv::randu(m2, -100, 100);
   cv::randu(m3, -100, 100);
@@ -80,8 +113,8 @@ static void cpumul(benchmark::State& state) {
 }
 
 static void cpumulmatrix(benchmark::State& state) {
-  cv::Mat m1(rows, cols, CV_64FC1), m2(cols, 1, CV_64FC1);
-  cv::Mat m3(rows, 1, CV_64FC1), m4(rows, 1, CV_64FC1);
+  cv::Mat m1(rows, cols, CV_TYPE), m2(rows2, cols2, CV_TYPE);
+  cv::Mat m3(rows, cols2, CV_TYPE), m4(rows, cols2, CV_TYPE);
   cv::randu(m1, -100, 100);
   cv::randu(m2, -100, 100);
   cv::randu(m3, -100, 100);
@@ -93,22 +126,23 @@ static void cpumulmatrix(benchmark::State& state) {
 
 static void gpugemm(benchmark::State& state) {
   cv::cuda::Stream stream;
-  cv::Mat tmp(rows, cols, CV_64FC1);
+  cv::Mat tmp(rows, cols, CV_TYPE);
   cv::cuda::GpuMat m1, m2, m3, m4;
 
   cv::randu(tmp, -100, 100);
   m1.upload(tmp);
 
-  tmp.create(cols, 1,CV_64FC1);
+  tmp.create(rows2, cols2,CV_TYPE);
   cv::randu(tmp, -100, 100);
   m2.upload(tmp);
 
-  tmp.create(rows, 1,CV_64FC1);
+  tmp.create(rows, cols2,CV_TYPE);
   cv::randu(tmp, -100, 100);
   m3.upload(tmp);
 
-//  cv::randu(tmp, -100, 100);
-//  m4.upload(tmp);
+  tmp.create(rows, cols2,CV_TYPE);
+  cv::randu(tmp, -100, 100);
+  m4.upload(tmp);
 
   for (auto _ : state) {
     cv::cuda::gemm(m1, m2, alpha, m3, belta, m4, 0, stream);
@@ -119,18 +153,18 @@ static void gpugemm(benchmark::State& state) {
 
 static void gpugemm2(benchmark::State& state) {
   cv::cuda::Stream stream;
-  cv::Mat tmp(rows, cols, CV_64FC1), cm1;
+  cv::Mat tmp(rows, cols, CV_TYPE), cm1;
   cv::cuda::GpuMat m1, m2, m3, m4;
 
   cv::randu(tmp, -100, 100);
   m1.upload(tmp);
   cm1 = tmp.clone();
 
-  tmp.create(cols, rows,CV_64FC1);
+  tmp.create(cols, rows,CV_TYPE);
   cv::randu(tmp, -100, 100);
   m2.upload(tmp);
 
-  tmp.create(rows, rows,CV_64FC1);
+  tmp.create(rows, rows,CV_TYPE);
   cv::randu(tmp, -100, 100);
   m3.upload(tmp);
 
@@ -143,18 +177,64 @@ static void gpugemm2(benchmark::State& state) {
   }
 }
 
-BENCHMARK(cpumul);
-BENCHMARK(cpugpumul);
-BENCHMARK(gpumul);
-BENCHMARK(gpumulnostream);
-BENCHMARK(cpugpumulnostream);
 
+void test_eigen_parallel(benchmark::State& state) {
+  LOG << "eigen thread = " << Eigen::nbThreads() << std::endl;
+  Eigen::Matrix<Scalar_, Eigen::Dynamic, Eigen::Dynamic> m1, m2, m3, m4;
+
+  m1.resize(rows, cols);
+  m1.setRandom();
+
+  m2.resize(rows2, cols2);
+  m2.setRandom();
+
+  m3.resize(rows, cols2);
+  m3.setRandom();
+
+  m4.resize(rows, cols2);
+  m4.setRandom();
+
+  for (auto _ : state) {
+    m4 = alpha * m1 * m2 + belta * m3;
+  }
+}
+
+void test_viennacl_linalg(benchmark::State& state) {
+  viennacl::matrix<Scalar_> m1(rows, cols), m2(rows2, cols2), m3(rows, cols2), m4(rows, cols2);
+  viennacl::tools::uniform_random_numbers<Scalar_> randomNumber;
+#define RandomViennacl(matrix) do { \
+  for (unsigned int i = 0; i < matrix.size1(); ++i)\
+    for (unsigned int j = 0; j < matrix.size2(); ++j)\
+      matrix(i,j) = randomNumber();\
+} while(0)
+
+
+  RandomViennacl(m1);
+  RandomViennacl(m2);
+  RandomViennacl(m3);
+
+#undef RandomViennacl
+  for (auto _ : state) {
+    m4 = alpha * viennacl::linalg::prod(m1, m2) + belta * m3;
+  }
+}
+
+//BENCHMARK(cpumul);
+//BENCHMARK(cpugpumul);
+//BENCHMARK(gpumul);
+//BENCHMARK(gpumulnostream);
+//BENCHMARK(cpugpumulnostream);
+//
+//BENCHMARK(gpugemm2);
 BENCHMARK(cpumulmatrix);
 BENCHMARK(gpugemm);
-BENCHMARK(gpugemm2);
+BENCHMARK(test_viennacl_linalg);
+BENCHMARK(test_eigen_parallel);
 
 //BENCHMARK_MAIN();
 int main(int argc, char** argv) {
+  Eigen::initParallel();
+  Eigen::setNbThreads(8);
   ::benchmark::Initialize(&argc, argv);
   if (::benchmark::ReportUnrecognizedArguments(argc, argv))
     return 1;
